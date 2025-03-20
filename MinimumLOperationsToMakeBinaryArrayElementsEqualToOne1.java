f6c96fc2abdc97137ff5800a956c2be6c0e2eec0
ank1299 <89583360+ank1299@users.noreply.github.com>
2025-03-20 10:21:07 +0530

    Create MinimumLOperationsToMakeBinaryArrayElementsEqualToOne1.java

---
+++ MinimumLOperationsToMakeBinaryArrayElementsEqualToOne1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int ans = 0;
+        //sliding window
+        for(int i = 0;i<nums.length -2 ;i++){          
+            if(nums[i] != 1){
+                nums[i] = 1^nums[i];
+                nums[i+1] = 1^nums[i+1];
+                nums[i+2] = 1^nums[i+2];
+                ans++;
+            }
+            
+        }
+        if(nums[nums.length -1] == 0 || nums[nums.length -2] == 0)return -1;
+        return ans;
+    }
+}
